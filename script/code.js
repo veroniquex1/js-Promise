ce7c6d2a5155bc543b7d39058dc0da7a083e60f5
veroniquex1 <vdrossouw@gmail.com>
2023-11-30 16:22:07 +0200

    final changes

--- script/code.js
+++ script/code.js
@@ -13,15 +13,17 @@ function addition (){
             reject(`Number 1 or Number 2 is not a Number`)
         }
         // switch (true) {
-        //     case typeof (num1) && (num2) = 'number':
+        //     case typeof (num1) == 'number' && typeof (num2) == 'number':
         //         let ans = num1 + num2
         //         resolve(`Success ${num1} and ${num2} = ${ans}`)
-                
+
         //         break;
-        //     case Number.isNaN(num1) && Number.isNaN(num2):
-        //         reject(`Number 1 and number 2 is not a number`)
+        //     case typeof(num1) != 'number':
+        //         reject(`Number 1 is not a number`)
         //         break;
-        
+        //     case typeof(num2) != 'number':
+        //         reject(`Number 2 is not a number`)
+        //         break;
         //     default:
         //         break;
         // }
