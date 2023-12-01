373326c1471b61a903eda8eeff1660c602b653e1
veroniquex1 <vdrossouw@gmail.com>
2023-12-01 09:01:45 +0200

    switch updates

--- script/code.js
+++ script/code.js
@@ -6,25 +6,29 @@ function addition (){
     let num2 = +document.getElementById('num2').value
     let output = document.querySelector('#output')
     let newPromise = new Promise( (resolve, reject)=>{
-        if( (typeof num1 == 'number') && (typeof num2 == 'number') ){
-            let ans = num1 + num2
-            resolve(`Sucess! The sum of ${num1} and ${num2} = ${ans}`)
-        }else {
-            reject(`Number 1 or Number 2 is not a Number`)
-        }
-        // switch (true) {
-        //     case typeof (num1) && (num2) = 'number':
-        //         let ans = num1 + num2
-        //         resolve(`Success ${num1} and ${num2} = ${ans}`)
-                
-        //         break;
-        //     case Number.isNaN(num1) && Number.isNaN(num2):
-        //         reject(`Number 1 and number 2 is not a number`)
-        //         break;
-        
-        //     default:
-        //         break;
+        // if( (typeof num1 == 'number') && (typeof num2 == 'number') ){
+            // let ans = num1 + num2
+            // resolve(`Sucess! The sum of ${num1} and ${num2} = ${ans}`)
+        // }else {
+            // reject(`Number 1 or Number 2 is not a Number`)
         // }
+        switch (true) {
+            case typeof (num1) && typeof (num2) == 'number':
+                let ans = num1 + num2
+                resolve(`Success! ${num1} and ${num2} = ${ans}`)
+
+              break;
+          case typeof (num1) == 'number' && typeof (num2) != 'number':
+              reject(`Number 2 is not a number`)
+              break;
+          case typeof (num2) == 'number' && typeof (num1) != 'number':
+              reject(`Number 1 is not a number`)
+              break;
+        
+      default:
+            output.textContent = 'Numbers 1 and 2 are not numbers'
+        break;
+        }
     })
     newPromise.then( 
         completed=> output.textContent= completed,
